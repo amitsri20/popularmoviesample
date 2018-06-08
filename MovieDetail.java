7486f2092958e6aec74c45f8294ebf5a88e94925
Amit Srivastava <amitsrivastava200892@gmail.com>
2018-06-08 23:43:38 +0530

    Create MovieDetail.java

---
+++ MovieDetail.java
@@ -0,0 +1,58 @@
+package com.crossphd.popularmovies;
+
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Parcel;
+import android.support.annotation.RequiresApi;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+import java.util.Objects;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MovieDetail extends AppCompatActivity {
+
+    public static final String EXTRA_POSITION = "extra_position";
+    private static final int DEFAULT_POSITION = -1;
+
+    @BindView(R.id.title_tv)
+    TextView title;
+    @BindView(R.id.synopsis_tv)
+    TextView synopsis;
+    @BindView(R.id.release_date_tv)
+    TextView releaseDate;
+    @BindView(R.id.rating_tv)
+    TextView rating;
+    @BindView(R.id.image_iv)
+    ImageView poster;
+
+
+    //    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.movie_detail);
+        ButterKnife.bind(this);
+
+//        Intent intent = getIntent();
+//        if (intent == null) {
+//            closeOnError();
+//        }
+
+
+        Movie movie = getIntent().getParcelableExtra("movie");
+//        Toast.makeText(this, movie.toString(), Toast.LENGTH_LONG).show();
+//        title.setText("title");
+        title.setText(movie.getmTitle());
+
+    }
+}
